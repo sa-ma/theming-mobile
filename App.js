3e9d5239e053a12bc333ae78b203cc515f6fae64
Samaila Bala <samailabalap@gmail.com>
2022-03-01 22:30:07 +0100

    create expo react native app

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ThemeProvider } from 'styled-components/native';
+import { FlatList } from 'react-native';
+import PostItem from './PostItem';
+import data from './data';
+import { Container, Header, ThemeButton, ThemeButtonText, TitleText } from './style';
+import { darkTheme, lightTheme } from './theme';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [theme, setTheme] = useState('light');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+        getTheme();
+    }, []);
+
+    const getTheme = async () => {
+        try {
+            const themeValue = await AsyncStorage.getItem('@theme');
+            if (themeValue) setTheme(themeValue);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const toggleTheme = async () => {
+        const themeValue = theme === 'dark' ? 'light' : 'dark';
+        try {
+            await AsyncStorage.setItem('@theme', themeValue);
+            setTheme(themeValue);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+            <Container>
+                <Header>
+                    <TitleText fontSize='24px'>Blog</TitleText>
+                    <ThemeButton>
+                        <ThemeButtonText onPress={() => toggleTheme()}>
+                            {theme === 'dark' ? 'Light' : 'Dark'} Mode
+                        </ThemeButtonText>
+                    </ThemeButton>
+                </Header>
+                <FlatList data={data} renderItem={PostItem} keyExtractor={(item) => item.id} />
+                <StatusBar style='auto' />
+            </Container>
+        </ThemeProvider>
+    );
+}
---
+++ theme.js
@@ -0,0 +1,16 @@
+const darkTheme = {
+    PRIMARY_COLOR: '#000',
+    SECONDARY_COLOR: '#73737d',
+    TITLE_COLOR: '#fff',
+    BACKGROUND_COLOR: '#111216',
+    BUTTON_COLOR: '#fff',
+};
+const lightTheme = {
+    PRIMARY_COLOR: '#fff',
+    SECONDARY_COLOR: '#73737d',
+    TITLE_COLOR: '#000',
+    BACKGROUND_COLOR: '#fff',
+    BUTTON_COLOR: '#000',
+};
+
+export { lightTheme, darkTheme };
